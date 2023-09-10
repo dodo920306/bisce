84b8f90dad825c355a04a6fa08128afff2741fa5
Kirin_Chu <74814435+dodo920306@users.noreply.github.com>
2023-09-10 09:23:34 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,6 +113,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
